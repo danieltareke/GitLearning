a9d400683f2982d3c2792bb6a95c9e64676b0b23
daniel tareke <daniel.tareke@gmail.com>
2021-07-02 10:08:38 +0300

    order management class added

---
+++ Order.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GitTestApp
+{
+    class Order
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Order management class");
+        }
+    }
+}
