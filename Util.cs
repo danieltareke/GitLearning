fd35740341d9cf35f67050112e1020bded61aff4
daniel tareke <daniel.tareke@gmail.com>
2021-07-02 09:55:52 +0300

    new class file added

---
+++ Util.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GitTestApp
+{
+    class Util
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test class");
+        }
+    }
+}
